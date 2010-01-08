7360a6e1647c8b799b10eec62223080e4304e4ff
David Balatero <david@bitwax.cd>
2010-01-07 17:31:48 -0800

    Added stats create method

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,11 @@
 class StatsController < ApplicationController
+  def create
+    @user = User.find_or_create_by_name(params[:user][:name])
+    @stat = Stat.new(:amount => params[:stat][:amount])
+    @stat.user = @user
+    @stat.save!
+
+    # 207 Multi-Status (WebDAV) (RFC 2518)
+    render :nothing => true, :status => 207
+  end
 end
--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,2 +1,3 @@
 class Stat < ActiveRecord::Base
+  belongs_to :user
 end
